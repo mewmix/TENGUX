99a6c1790679b4241e663b213692e723aa359ab9
mewmix <42463809+mewmix@users.noreply.github.com>
2018-08-20 15:47:52 -0700

    Update CryptoNoteConfig.h

--- src/CryptoNoteConfig.h
+++ src/CryptoNoteConfig.h
@@ -211,9 +211,9 @@ const static boost::uuids::uuid CRYPTONOTE_NETWORK =
 };
 
 const char* const SEED_NODES[] = {
-  "35.203.155.214:11897",//awp1
+  "35.203.139.161:11897",//awp1
   "35.196.46.195:11897", //awp2
-  "35.227.127.20:11897", //instance1
+  "35.196.63.89:11897", //instance1
  
 };
 } // <3 CryptoNote & TURTLE - Alexander Klein 
